6a07e1f27cf1ad43d7380bd65fb99d5ba3e27da3
xandydrv <149505974+xandydrv@users.noreply.github.com>
2025-06-30 20:30:07 -0300

    script.js

--- script.js
+++ script.js
@@ -1,113 +1,125 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
 const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
-            }
-        ]
-    },
-    {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
-            {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
-            },
-            {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
-        ]
-    },
-    {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
-];
+  {
+    enunciado:
+      "Você está dirigindo em uma estrada deserta durante uma tempestade quando seu carro quebra. À distância, você vê uma casa antiga e aparentemente abandonada. A chuva está ficando mais forte. O que você faz?",
+    alternativas: [
+      {
+        texto: "Corro até a casa para me abrigar da tempestade",
+        afirmacao: "Você decidiu buscar abrigo na casa misteriosa, ignorando os sinais de perigo. ",
+      },
+      {
+        texto: "Fico no carro e espero a tempestade passar",
+        afirmacao: "Você tentou ficar seguro no carro, mas o frio e a tempestade te forçaram a buscar abrigo. ",
+      },
+    ],
+  },
+  {
+    enunciado:
+      "Ao se aproximar da casa, você nota que a porta da frente está entreaberta, balançando com o vento. Há uma luz fraca vinda do interior. Você também percebe uma entrada lateral que parece levar ao porão. Por onde você entra?",
+    alternativas: [
+      {
+        texto: "Entro pela porta da frente, seguindo a luz",
+        afirmacao:
+          "Você escolheu o caminho mais óbvio, entrando pela porta principal onde uma presença sinistra já havia notado sua chegada. ",
+      },
+      {
+        texto: "Vou pela entrada lateral para o porão",
+        afirmacao:
+          "Você optou por uma entrada mais discreta, descendo ao porão onde segredos sombrios aguardavam nas sombras. ",
+      },
+    ],
+  },
+  {
+    enunciado:
+      "Dentro da casa, você ouve passos vindos do andar de cima, mas não vê ninguém. De repente, todas as portas da casa se fecham com um estrondo. Você encontra duas opções: uma escada que leva ao segundo andar ou uma porta que parece levar à cozinha. O que você escolhe?",
+    alternativas: [
+      {
+        texto: "Subo as escadas para investigar os passos",
+        afirmacao:
+          "Sua curiosidade te levou ao encontro direto com a entidade que habitava a casa, confrontando o terror de frente. ",
+      },
+      {
+        texto: "Vou para a cozinha procurar uma saída",
+        afirmacao: "Você tentou encontrar uma rota de fuga, mas descobriu que a casa tinha outros planos para você. ",
+      },
+    ],
+  },
+  {
+    enunciado:
+      "Você encontra um diário antigo sobre uma mesa. As páginas falam sobre rituais sombrios e uma família que desapareceu misteriosamente há 50 anos. Suddenly, você ouve uma voz sussurrando seu nome. A voz parece vir de duas direções diferentes. O que você faz?",
+    alternativas: [
+      {
+        texto: "Sigo a voz que vem de cima, ela parece mais clara",
+        afirmacao: "Você seguiu a voz sedutora que te levou diretamente para a armadilha preparada há décadas. ",
+      },
+      {
+        texto: "Sigo a voz que vem do porão, talvez seja uma saída",
+        afirmacao:
+          "Você desceu em direção à fonte do mal, onde os rituais antigos ainda ecoavam pelas paredes úmidas. ",
+      },
+    ],
+  },
+  {
+    enunciado:
+      "Você se depara com uma figura sombria que bloqueia seu caminho. Ela oferece um acordo: 'Você pode sair, mas deve deixar algo seu para trás - sua memória desta noite ou sua capacidade de sentir medo.' A figura estende duas mãos, cada uma brilhando com uma energia diferente. Qual você escolhe?",
+    alternativas: [
+      {
+        texto: "Aceito perder minha memória desta noite",
+        afirmacao:
+          "Você escolheu esquecer, mas algumas experiências deixam marcas na alma que a mente não consegue apagar completamente. Você saiu da casa, mas uma parte sombria permaneceu com você para sempre.",
+      },
+      {
+        texto: "Aceito perder minha capacidade de sentir medo",
+        afirmacao:
+          "Você escolheu viver sem medo, mas descobriu que o medo é o que nos mantém humanos. Agora você caminha pelo mundo sem a proteção que o medo oferece, atraindo perigos que outros evitariam instintivamente.",
+      },
+    ],
+  },
+]
 
-
-let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
+let atual = 0
+let perguntaAtual
+let historiaFinal = ""
 
 function mostraPergunta() {
-    if (atual >= perguntas.length) {
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
+  if (atual >= perguntas.length) {
+    mostraResultado()
+    return
+  }
+  perguntaAtual = perguntas[atual]
+  caixaPerguntas.textContent = perguntaAtual.enunciado
+  caixaAlternativas.textContent = ""
+  mostraAlternativas()
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button")
+    botaoAlternativas.textContent = alternativa.texto
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+    caixaAlternativas.appendChild(botaoAlternativas)
+  }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
+  const afirmacoes = opcaoSelecionada.afirmacao
+  historiaFinal += afirmacoes + " "
+  atual++
+  mostraPergunta()
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
+  caixaPerguntas.textContent = "Seu destino foi selado..."
+  textoResultado.textContent =
+    historiaFinal +
+    " A casa abandonada permanece lá, esperando pelo próximo visitante desavisado. Algumas escolhas ecoam pela eternidade, e a sua acabou de se tornar parte da lenda sombria daquele lugar amaldiçoado."
+  caixaAlternativas.textContent = ""
 }
 
-mostraPergunta();
+mostraPergunta()
